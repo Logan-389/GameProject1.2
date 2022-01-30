c7e02750ef8353a2d81338bcc4ae646e6d352dea
rider <rider_oz@mac.com>
2022-01-29 20:12:26 -0500

    Added basic inventory menu. The key is I.

---
+++ GameProject1.2/Assets/Scripts/Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Inventory", menuName = "Inventory")]
+public class Inventory : ScriptableObject
+{
+    
+    
+
+}
