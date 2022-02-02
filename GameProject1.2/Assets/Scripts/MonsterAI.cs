542de1b855b60a268597b020119d05366b1b199e
rider <rider_oz@mac.com>
2022-02-02 14:02:38 -0500

    Added hiding in bushes and stuff.

--- GameProject1.2/Assets/Scripts/MonsterAI.cs
+++ GameProject1.2/Assets/Scripts/MonsterAI.cs
@@ -6,52 +6,69 @@ using UnityEngine.AI;
 public class MonsterAI : MonoBehaviour
 {
     
+
     NavMeshAgent agent;
 
     GameObject player;
 
-    public float randTime;
-
     GameObject monsterHouse;
 
     private bool hunting;
+
+    IEnumerator activeHuntCycle = null;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
-        randTime = Random.Range(10, 60);
         monsterHouse = GameObject.Find("MonsterHouse");
-        hunting = false;
-        agent.destination = monsterHouse.transform.position;
+        StartHuntCycle();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        randTime -= Time.deltaTime;
+        
         Debug.Log(hunting);
 
-        //update agent location
-        if (hunting == false)
-        {
-            agent.destination = monsterHouse.transform.position;
-        }
-        else if (hunting == true)
+        if (hunting == true)
         {
             agent.destination = player.transform.position;
         }
 
-        //update if we are hunting or not
-        if (randTime <= 0.0f && hunting == true)
+    }
+
+    IEnumerator HuntCycle()
+    {
+        while (true)
         {
             hunting = false;
-            randTime = Random.Range(10, 60);
-        } else if (randTime <= 0.0f && hunting == false)
-        {
+            agent.destination = monsterHouse.transform.position;
+            yield return new WaitForSeconds(Random.Range(10, 60));
             hunting = true;
-            randTime = Random.Range(10, 60);
+            yield return new WaitForSeconds(Random.Range(10, 60));
         }
     }
+
+
+    public void StartHuntCycle()
+    {
+        Debug.Log("Starting Hunt Cycle");
+        activeHuntCycle = HuntCycle();
+        StartCoroutine(activeHuntCycle);
+    }
+
+    public void StopHuntCycle()
+    {
+        Debug.Log("Stoping Hunt Cycle");
+        if (activeHuntCycle != null)
+        {
+            StopCoroutine(activeHuntCycle);
+        }
+        activeHuntCycle = null;
+        hunting = false;
+        agent.destination = monsterHouse.transform.position;
+    }
+
 }
